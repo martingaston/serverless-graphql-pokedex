ee4e31788637363456a70bac919587e016bb26b4
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-07-15 21:51:40 +0100

    add Pokemon schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,20 @@ const { ApolloServer, gql } = require('apollo-server-lambda')
 const typeDefs = gql`
   type Query {
     hello: String!
+    allPokemon: [Pokemon]!
+    pokemon(id: ID!): Pokemon
+  }
+
+  type Mutation {
+    likePokemon(id: ID!): Pokemon
+  }
+
+  type Pokemon {
+    id: ID!
+    name: String
+    type: [String]
+    sprite: String
+    likes: Int
   }
 `
 
