70120f71fd68c814ff092fb5eec67d9f8aeda1e3
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-07-16 21:56:19 +0100

    add pokemon resolver with local db lookup

--- backend/src/resolvers.js
+++ backend/src/resolvers.js
@@ -1,5 +1,30 @@
+const AWS = require('aws-sdk')
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient({endpoint: 'http://dynamodb:8000'})
+
+const getPokemon = id => new Promise((resolve, reject) => {
+  params = {
+    "TableName": "pokedex",
+    "Key": {
+      "id": parseInt(id),
+    }
+  }
+
+  dynamoDb.get(params, function(err, data) {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(data)
+    }
+  })
+})
+
 exports.resolvers = {
   Query: {
-    hello: () => 'Hello from GraphQL!'
+    hello: () => 'Hello from GraphQL!',
+    pokemon: (parent, args, context) => getPokemon(args.id).then(data => data.Item)
+  },
+  Pokemon: {
+    type: (parent) => parent.type.values
   }
 }
