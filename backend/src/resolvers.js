f3ef3bcdbc5c2b6f970d000c9c8160c02c54a58b
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-07-16 22:59:56 +0100

    add allPokemon query to resolvers

--- backend/src/resolvers.js
+++ backend/src/resolvers.js
@@ -3,21 +3,30 @@ const AWS = require('aws-sdk')
 const dynamoDb = new AWS.DynamoDB.DocumentClient({endpoint: 'http://dynamodb:8000'})
 
 const getPokemon = id => {
-  params = {
+  const params = {
     "TableName": "pokedex",
     "Key": {
       "id": parseInt(id),
     }
   }
 
-  return dynamoDb.get(params).promise()
+  return dynamoDb.get(params).promise().then(data => data.Item)
+}
+
+const getAllPokemon = () => {
+  const params = {
+    "TableName": "pokedex"
+  }
+
+  return dynamoDb.scan(params).promise().then(data => data.Items)
 }
 
 
 exports.resolvers = {
   Query: {
     hello: () => 'Hello from GraphQL!',
-    pokemon: (parent, args, context) => getPokemon(args.id).then(data => data.Item)
+    allPokemon: (parent, args, context) => getAllPokemon(),
+    pokemon: (parent, args, context) => getPokemon(args.id)
   },
   Pokemon: {
     type: (parent) => parent.type.values
