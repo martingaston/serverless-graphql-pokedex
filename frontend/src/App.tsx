744f94dd4d83882549e8071baae1cbade9bb4569
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-07-17 13:57:11 +0100

    remove template app and add hardcoded html alongside tailwind css dependency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="w-64 mb-6">
+        <div className="bg-white rounded-lg p-6">
+          <img className="h-32 mx-auto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" />
+          <div className="text-center">
+            <h2 className="text-xl">Ditto</h2>
+            <div className="text-purple-500">#132</div>
+            <div className="text-gray-600">Normal</div>
+          </div>
+          <div className="flex flex-col mt-4 items-center">
+            <svg className="grow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path className="normal" d="M17.867 3.493l4.133 3.444v5.127l-10 8.333-10-8.334v-5.126l4.133-3.444 5.867 3.911 5.867-3.911zm.133-2.493l-6 4-6-4-6 5v7l12 10 12-10v-7l-6-5z" />
+              <path className="hover" d="M18 1l-6 4-6-4-6 5v7l12 10 12-10v-7z" />
+            </svg>
+            <div className="mt-2 text-gray-500">5</div>
+          </div>
+        </div>
+      </section>
+      <section className="w-64 mb-6">
+        <div className="bg-white rounded-lg p-6">
+          <img className="h-32 mx-auto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" />
+          <div className="text-center">
+            <h2 className="text-xl">Pikachu</h2>
+            <div className="text-purple-500">#25</div>
+            <div className="text-gray-600">Electric</div>
+          </div>
+          <div className="flex flex-col mt-4 items-center">
+            <svg className="grow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path className="normal" d="M17.867 3.493l4.133 3.444v5.127l-10 8.333-10-8.334v-5.126l4.133-3.444 5.867 3.911 5.867-3.911zm.133-2.493l-6 4-6-4-6 5v7l12 10 12-10v-7l-6-5z" />
+              <path className="hover" d="M18 1l-6 4-6-4-6 5v7l12 10 12-10v-7z" />
+            </svg>
+            <div className="mt-2 text-gray-500">128</div>
+          </div>
+        </div>
+      </section>
+      <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    </>
   );
 }
 
