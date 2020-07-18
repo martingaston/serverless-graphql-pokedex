b33c1719c3f601b7ce9e2d03a18d53e37a03276d
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-07-18 17:46:40 +0100

    add GraphQL mutation for liking pokemon

--- frontend/src/PokemonCard.tsx
+++ frontend/src/PokemonCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { gql, useMutation } from '@apollo/client'
 
 export type PokemonCardInfo = {
     name: string,
@@ -8,7 +9,17 @@ export type PokemonCardInfo = {
     likes: number
 }
 
+const LIKE_POKEMON = gql`
+mutation likePokemon($id: ID!) {
+    likePokemon(id: $id) {
+      id
+      likes
+    }
+  }
+`
+
 const PokemonCard = ({ name, id, type, sprite, likes }: PokemonCardInfo) => {
+    const [addLike] = useMutation(LIKE_POKEMON);
     return (
         <section className="w-64 mb-6">
             <div className="bg-white rounded-lg p-6">
@@ -19,11 +30,13 @@ const PokemonCard = ({ name, id, type, sprite, likes }: PokemonCardInfo) => {
                     <div className="text-gray-600">{type}</div>
                 </div>
                 <div className="flex flex-col mt-4 items-center">
-                    <svg className="grow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path className="normal" d="M17.867 3.493l4.133 3.444v5.127l-10 8.333-10-8.334v-5.126l4.133-3.444 5.867 3.911 5.867-3.911zm.133-2.493l-6 4-6-4-6 5v7l12 10 12-10v-7l-6-5z" />
-                        <path className="hover" d="M18 1l-6 4-6-4-6 5v7l12 10 12-10v-7z" />
-                    </svg>
-                    <div className="mt-2 text-gray-500">{likes}</div>
+                    <button onClick={() => addLike({ variables: { id } })}>
+                        <svg className="grow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path className="normal" d="M17.867 3.493l4.133 3.444v5.127l-10 8.333-10-8.334v-5.126l4.133-3.444 5.867 3.911 5.867-3.911zm.133-2.493l-6 4-6-4-6 5v7l12 10 12-10v-7l-6-5z" />
+                            <path className="hover" d="M18 1l-6 4-6-4-6 5v7l12 10 12-10v-7z" />
+                        </svg>
+                        <div className="mt-2 text-gray-500">{likes}</div>
+                    </button>
                 </div>
             </div>
         </section>
